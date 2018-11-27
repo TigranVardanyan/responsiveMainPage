d2446d9bb37756d8c6c46a73936f1dc68386eba1
Tigran <tig.vardanyan.001@gmail.com>
2018-11-27 13:33:14 +0400

    template 2.2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,34 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync'),
-	del = require('del');
+	del = require('del'),
+	autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('mytask' , function() {
-	console.log('hello');
-	});
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*.+(sass|scss)') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
 
-gulp.task('sass' , function() {
-	return gulp.src('app/sass/**/*.+(sass|scss)')
-			.pipe(sass())
-			.pipe(gulp.dest('app/css/'))
-	})
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
 
-gulp.task('watch' , function() {
-	gulp.watch('app/sass/**/*' , ['sass'])
-	});
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
 
-gulp.task('clean' , function() {
-	return del.sync('dist'); //удальяем папку дист перед сборкой
-	});
-
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('app/sass/**/*.+(sass|scss)', ['sass']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
+});
 gulp.task('build' , ['clean' , 'sass'], function() {
 	var buildCss = gulp.src([
 		'app/css/main.css'
@@ -38,4 +46,7 @@ gulp.task('build' , ['clean' , 'sass'], function() {
 		.pipe(gulp.dest('dist/'));
 	var logic = gulp.src([
 		'app/logic/**/*.*'])
-		.pipe(gulp.dest('dist/logic'))
+		.pipe(gulp.dest('dist/logic'));
+	var img = gulp.src([
+		'app/img/**/*.*'])
+		.pipe(gulp.dest('dist/img'));
