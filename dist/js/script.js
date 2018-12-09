b63e624b31ef8d87f56475d57dcacfbd4c164015
Tigran <tig.vardanyan.001@gmail.com>
2018-12-09 19:25:10 +0400

    intermediate
    для профилактики

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,7 +4,7 @@ var page = document.querySelectorAll('.page');
 var home = document.querySelectorAll('.home');
 var portfolio = document.querySelectorAll('.portfolio');
 var service = document.querySelectorAll('.service');
-var resumeblog = document.querySelectorAll('.resumeblog');
+var resume = document.querySelectorAll('.resume');
 var blog = document.querySelectorAll('.blog');
 var contact = document.querySelectorAll('.contact');
 var extra = document.querySelectorAll('.extra');
@@ -38,20 +38,20 @@ link[2].addEventListener('click' , function(e) {
 		page[i].classList.remove('current_page_show');
 		page[i].classList.add('current_page_hide');
 	}
-	for(var i = 0; i < home.length; i++) {
-		home[i].classList.remove('current_page_hide');
-		home[i].classList.add('current_page_show');
+	for(var i = 0; i < service.length; i++) {
+		service[i].classList.remove('current_page_hide');
+		service[i].classList.add('current_page_show');
 	}
 })
 
 link[3].addEventListener('click' , function(e) {
-	for(var i = 0; i < page.length; i++) {
-		page[i].classList.remove('current_page_show');
-		page[i].classList.add('current_page_hide');
+	for(var i = 0; i < resume.length; i++) {
+		resume[i].classList.remove('current_page_show');
+		resume[i].classList.add('current_page_hide');
 	}
-	for(var i = 0; i < home.length; i++) {
-		home[i].classList.remove('current_page_hide');
-		home[i].classList.add('current_page_show');
+	for(var i = 0; i < blog.length; i++) {
+		blog[i].classList.remove('current_page_hide');
+		blog[i].classList.add('current_page_show');
 	}
 })
 
@@ -60,9 +60,9 @@ link[4].addEventListener('click' , function(e) {
 		page[i].classList.remove('current_page_show');
 		page[i].classList.add('current_page_hide');
 	}
-	for(var i = 0; i < home.length; i++) {
-		home[i].classList.remove('current_page_hide');
-		home[i].classList.add('current_page_show');
+	for(var i = 0; i < contact.length; i++) {
+		contact[i].classList.remove('current_page_hide');
+		contact[i].classList.add('current_page_show');
 	}
 })
 
@@ -72,7 +72,7 @@ link[5].addEventListener('click' , function(e) {
 		page[i].classList.add('current_page_hide');
 	}
 	for(var i = 0; i < home.length; i++) {
-		home[i].classList.remove('current_page_hide');
-		home[i].classList.add('current_page_show');
+		extra[i].classList.remove('current_page_hide');
+		extra[i].classList.add('current_page_show');
 	}
 })
