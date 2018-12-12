aed2bfc0e4279a0407c2c2cc51f66d7149dab190
Tigran <tig.vardanyan.001@gmail.com>
2018-12-12 16:05:24 +0400

    Проходной

--- dist/head.php
+++ dist/head.php
@@ -25,7 +25,7 @@
 	<noscript><div><img src="https://mc.yandex.ru/watch/51528389" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 	<!-- /Yandex.Metrika counter -->
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=0.8">
 	<title>MyDailyCode.ru</title>
 	<!--  font awesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
