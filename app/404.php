f7dcbaeb02065ee9cd0da668743e54a2dab18019
Tigran <tig.vardanyan.001@gmail.com>
2018-12-22 13:52:24 +0400

    404

--- app/404.php
+++ app/404.php
@@ -2,19 +2,19 @@
 <html lang="en">
 <?php require_once('head.php') ?>
 <body>
-	<div class="container-fluid wrapper">
+	<div class="container wrapper">
 		<div class="row justify-content-center">
-			<div class="container-fluid content col-12">
+			<?php require_once('navbar.php'); ?>
+		</div>
+		<div class="row justify-content-center">
+			<div class="container content col-12">
 				<div class="row align-items-center">
 					<div class="content_heaer cChange col-12">
 						<h1>Oops!!!</h1> <br>
 					</div>
 					<div class="container-fluid content_main col-12">
 						<div class="">
-							<a class="col-lg-6" href="index.php">Home page</a>
-						</div>
-						<div class="">
-							<h4 style="padding: 0px" class="col-lg-6">404: page not found</h4>
+							<h4 style="padding: 0px" class="col-lg">404: page not found</h4>
 						</div>
 					</div>
 				</div>
